4bec4a2fcd669b909997b08b1a3f962473e36902
Mark Hiserodt <mark.hiserodt@avanade.com>
2020-06-23 15:15:06 -0700

    tests page enhancements

--- Central Medical Laboratory/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Central Medical Laboratory/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit, OnDestroy{
   menuPage: string;
   searchText: string;
 
-  constructor(private router: Router) {
+  private routeSubscription: Subscription;
 
-  }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-
+    this.routeSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (this.activatedRoute.snapshot.children[0].url[0]) {
+          this.menuPage = this.activatedRoute.snapshot.children[0].url[0].path;
+        } else {
+          this.menuPage = 'home';
+        }
+      }
+    });
   }
 
   onSearchClick() {
-    this.router.navigate(['/tests', {search: this.searchText}]);
+    if (this.searchText) {
+      this.router.navigate(['/tests', {search: this.searchText}]);
+    } else {
+      this.router.navigate(['/tests']);
+    }
   }
 
   ngOnDestroy() {
-
+    this.routeSubscription.unsubscribe();
   }
 }
--- Central Medical Laboratory/ClientApp/src/app/tests/tests.component.ts
+++ Central Medical Laboratory/ClientApp/src/app/tests/tests.component.ts
@@ -1,45 +1,126 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tests',
   templateUrl: './tests.component.html',
   styleUrls: ['./tests.component.scss']
 })
-export class TestsComponent implements OnInit {
-  searchText: string;
+export class TestsComponent implements OnInit, OnDestroy {
+  searchText: string = '';
   tests: any = [
     { id: 1, name: 'alpha test' },
     { id: 2, name: 'beta test' },
     { id: 3, name: 'gamma test' },
     { id: 4, name: 'delta test' },
-    { id: 5, name: 'episilon test' },
+    { id: 5, name: 'epsilon test' },
     { id: 6, name: 'zeta test' },
     { id: 7, name: 'eta test' },
     { id: 8, name: 'theta test' },
-    { id: 9, name: 'iota test' }
+    { id: 9, name: 'iota test' },
+    { id: 10, name: 'test1' },
+    { id: 11, name: 'test2' },
+    { id: 12, name: 'test3' },
+    { id: 13, name: 'alpha2' }
   ];
-  filteredTests: any = {};
+  filteredTests: any = [];
 
-  constructor(private route: ActivatedRoute) { }
+  alphabet: any[] = [
+    {value: 'a', selected: false},
+    {value: 'b', selected: false},
+    {value: 'c', selected: false},
+    {value: 'd', selected: false},
+    {value: 'e', selected: false},
+    {value: 'f', selected: false},
+    {value: 'g', selected: false},
+    {value: 'h', selected: false},
+    {value: 'i', selected: false},
+    {value: 'j', selected: false},
+    {value: 'k', selected: false},
+    {value: 'l', selected: false},
+    {value: 'm', selected: false},
+    {value: 'n', selected: false},
+    {value: 'o', selected: false},
+    {value: 'p', selected: false},
+    {value: 'q', selected: false},
+    {value: 'r', selected: false},
+    {value: 's', selected: false},
+    {value: 't', selected: false},
+    {value: 'u', selected: false},
+    {value: 'v', selected: false},
+    {value: 'w', selected: false},
+    {value: 'x', selected: false},
+    {value: 'y', selected: false},
+    {value: 'z', selected: false},
+  ];
+
+  private paramsSubscription: Subscription;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      if(params['search']) {
+    this.paramsSubscription = this.route.params.subscribe(params => {
+      if (params['search']) {
         this.searchText = params['search'];
         this.doSearch();
+      } else {
+        this.searchText = '';
+        this.filteredTests = this.tests;
       }
-    })
-  }
-
-  doSearch() {
-    this.filteredTests = this.tests.filter(test => {
-      return (test.name.indexOf(this.searchText) > -1);
     });
   }
 
-  ngOnDestroy() {
+  doSearch() {
+    if (!this.isLetterSelected()) {
+      this.filteredTests = this.tests.filter(test => {
+        return (test.name.indexOf(this.searchText) > -1);
+      });
+    } else {
+      this.filteredTests = [];
+      this.alphabet.forEach(letter => {
+        if (letter.selected) {
+          let filteredTestsTemp = this.tests.filter(test => {
+            return (test.name.indexOf(letter.value) == 0);
+          });
+          this.filteredTests = [...this.filteredTests, ...filteredTestsTemp];
+        }
+      });
+      this.filteredTests = this.filteredTests.filter(test => {
+        return (test.name.indexOf(this.searchText) > -1);
+      });
+    }
+  }
 
+  onLetterClick(letter: any) {
+    this.unselectLetters();
+    letter.selected = !letter.selected;
+    this.doSearch();
+  }
+
+  removeFilter() {
+    this.searchText = '';
+    this.doSearch();
+  }
+
+  private unselectLetters() {
+    this.alphabet.forEach(letter => {
+      letter.selected = false;
+    });
+  }
+
+  private isLetterSelected(): boolean {
+    let found: boolean = false;
+    this.alphabet.forEach(letter => {
+      if (letter.selected) {
+        found = true;
+      }
+    });
+    return found;
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
