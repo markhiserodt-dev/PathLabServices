0e043b29759adecbf3e02dbb9effecd764168c18
Mark Hiserodt <mark.hiserodt@avanade.com>
2020-07-09 06:58:12 -0700

    publishing error fix

--- Central Medical Laboratory/ClientApp/src/app/animations.ts
+++ Central Medical Laboratory/ClientApp/src/app/animations.ts
@@ -2,34 +2,52 @@ import { trigger, transition, group, query, style, animate, animateChild } from 
 
 export const slideInAnimation =
   trigger('routeSlideAnimation', [
-    transition(':increment', slideTo('right') ),
-    transition(':decrement', slideTo('left') ),
-  ]);
-
-function slideTo(direction) {
-  const optional = { optional: true };
-  return [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        [direction]: 0,
-        width: '100%'
-      })
-    ], optional),
-    query(':enter', [
-      style({ [direction]: '-100%'})
-    ]),
-    query(':leave', animateChild()),
-    group([
-      query(':leave', [
-        animate('500ms ease-out', style({ [direction]: '100%'}))
-      ], optional),
+    transition(':increment', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: '100%'
+        })
+      ], { optional: true }),
       query(':enter', [
-        animate('500ms ease-out', style({ [direction]: '0%'}))
-      ])
-    ]),
-    query(':enter', animateChild()),
-  ];
-}
+        style({ right: '-100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('500ms ease-out', style({ right: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('500ms ease-out', style({ right: '0%' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ] ),
+    transition(':decrement', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ left: '-100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('500ms ease-out', style({ left: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('500ms ease-out', style({ left: '0%' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ] ),
+  ]);
